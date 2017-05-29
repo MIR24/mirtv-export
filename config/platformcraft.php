e9fd6e162ffc9d3b9f6291934a91e5837fc7c5c2
barantaran <yourchev@gmail.com>
2017-05-29 18:49:21 +0300

    Use Platformcraft CDN Library

---
+++ config/platformcraft.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    "apiuserid" => env('PC_API_USER_ID',"vUt5JhFF_Gz80i9KU-LteEzbI5mJZkHhOOFgKrYbOYE="),
+    "hmackey" => env('PC_HMAC_KEY',"8uYAzx0-sQrOquBKoh4kWiqILnbUk0bpL0fWSbsYHQQ=")
+];
