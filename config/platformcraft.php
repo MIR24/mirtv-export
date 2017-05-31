c7533c084836894bc73a8cf62111b05f0ef7f63d
barantaran <yourchev@gmail.com>
2017-05-31 15:23:59 +0300

    Add mirtv files local storage into export configuration

--- config/platformcraft.php
+++ config/platformcraft.php
@@ -2,5 +2,6 @@
 
 return [
     "apiuserid" => env('PC_API_USER_ID',"vUt5JhFF_Gz80i9KU-LteEzbI5mJZkHhOOFgKrYbOYE="),
-    "hmackey" => env('PC_HMAC_KEY',"8uYAzx0-sQrOquBKoh4kWiqILnbUk0bpL0fWSbsYHQQ=")
+    "hmackey" => env('PC_HMAC_KEY',"8uYAzx0-sQrOquBKoh4kWiqILnbUk0bpL0fWSbsYHQQ="),
+    "localvideopath" => env('MTV_VIDEO_FILE_PATH',"/home/web/double.mirtv.ru/public/files/video/")
 ];
